9f4997b0c341089b62b372b03d6e8a6e6c609f54
parksol <thfdl0317@naver.com>
2022-02-23 16:42:59 +0900

    SASS문법 돌입

--- src/App.js
+++ src/App.js
@@ -359,8 +359,10 @@ function App() {
                 현업에 종사하는 기획자, 디자이너, 마케팅, <br/>
                 개발자들이 모여 진행하고 있는 MVP 서비스 중고거래 플랫폼을 개발 중입니다.<br/>
                 <br/>
-                React와 Typescript를 사용하여 UI를 구현, <br/>
-                팀 블로그 형태로 운영 중이며, 초기 기획부터 런칭까지의 절차를 밟으며 협업할 때의 알아야할 다양한 지식, 정보를 배울 수 있었던 프로젝트였습니다.
+                SpringBoot와 JPA를 활용, Spring Security를 배우며 <br/>
+                적용할 수 있었고 팀 블로그 형태로 운영 중이며, 
+                <br/>
+                초기 기획부터 런칭까지의 절차를 밟으며 협업할 때의 알아야할 지식, 정보를 배울 수 있었던 프로젝트였습니다.
               
               </div>
                 <hr/>
@@ -371,17 +373,17 @@ function App() {
                 <li><i class="fas fa-fill-drip"></i> Front-end</li>
                 <li><i class="fas fa-fill-drip"></i> Back-end</li>
                 <li><i class="fas fa-fill-drip"></i> 블로그</li>
-                <li><i class="fas fa-fill-drip"></i> 사이트</li>
+                <li><i class="fas fa-fill-drip"></i> Web-Site</li>
               </ul>
               </div>
 
               <div className='siteSkillBody'>
               <ul>
-                        <li>검색/필터/구매문의</li>
+                        <li>검색/필터/구매문의 등</li>
                         <li>React, TypeScript</li>
-                        <li>SpringBoot, Java, MySQL</li>
-                        <li>https://blog.naver.com/boardgetddo_team/</li>
-                        <li>https://blog.naver.com/boardgetddo_team/</li>
+                        <li>SpringBoot, JPA, Java, MySQL</li>
+                        <li><a href='https://blog.naver.com/boardgetddo_team'>보드살롱 blog</a></li>
+                        <li><a href='#'>보드살롱</a></li>
                       </ul>
               </div>
             </div>
@@ -408,8 +410,9 @@ function App() {
                 <div className='siteContent'>
                 지금 보시는 웹사이트가 해당 사이트이며 <br/>
                 React에 배웠던 것을 활용하며 
-                레이아웃을 작업, TypeScript와 Sass를 사용하면서 프론트엔드 개발을 더 깊숙히
-                경험할 수 있던 프로젝트입니다.
+                레이아웃을 작업,
+                <br/> Sass과 styled-components를 사용하면서 기존 css작업을  넘어 다양한 프론트엔드 개발을 더 깊숙히
+                경험할 수 있었던 프로젝트입니다.
                 </div>
 
                 <hr/>
@@ -420,8 +423,7 @@ function App() {
                 <li><i class="fas fa-fill-drip"></i> 주요기능</li>
                 <br/><br/>
                 <li><i class="fas fa-fill-drip"></i> Front-end</li>
-                
-                <li><i class="fas fa-fill-drip"></i> Git-hub</li>
+                <li><i class="fas fa-fill-drip"></i> Web-Site</li>
               </ul>
               </div>
 
@@ -429,7 +431,7 @@ function App() {
               <ul>
                         <li>자기소개, 인적 사항, 보유 기술, 프로젝트/교육 경험, 업무 경력, 활동사이트등을 간단히 소개</li>
                         <li>React, TypeScript, SASS</li>
-                        <li className='git'>https://github.com/SOL-hub/SOL-sProtfolio</li>
+                        <li><a href='#'>SOL's Protfolio</a></li>
                       </ul>
               </div>
             </div>
@@ -452,14 +454,12 @@ function App() {
 
             <div className='ProContent'>
                 <div className='siteContent'>
-                통신서비스를 설치한 곳의 시설현황, 가공거리, 간선망 등을 검색, 조회하는 웹 사이트입니다. <br/>
-                <br/>각 목차별 UI들의 레이아웃을 제이쿼리와 CSS로 구현하며 웹 서비스에 주요 기능들을 나타낼 때 직관적인 사용을 할 수 있도록 구성할 수 있는 기본적인 문법들을 배울 수 있었습니다. <br/>
-                
+                통신서비스를 설치한 곳의 시설현황, 가공거리, 간선망 등을 검색, 조회하는 웹 사이트입니다. 
                 <br/>
-                각 UI마다의 기능들을 모듈화하는 방식, mybatis의 sql을 가져와 수정, 조회하며 MVC패턴의 흐름을 실무에서 깊이 파악할 수 있었고 
-                
                 <br/>
-                ajax인 비동기적 통신방식으로 새로 로딩없이 원하는 데이터만 보여줄 수 있게 구현하는 방법을 자세히 배울 수 있었던 경험이었습니다.
+                각 UI마다의 기능들을 모듈화하는 방식, mybatis의 sql을 가져와 수정, 조회하며 MVC패턴의 흐름을 실무에서 파악할 수 있었고 
+                ajax인 비동기적 통신방식으로 새로 로딩없이 원하는 데이터만 보여줄 수 있게 구현, 
+                <br/>각 목차별 UI들의 레이아웃을 JQuery와 CSS로 구현하며 웹 서비스에 주요 기능들을 구성하는 문법들을 배울 수 있었던 경험이었습니다. <br/>
                 </div>
 
                 <hr/>
@@ -470,16 +470,16 @@ function App() {
                 <li><i class="fas fa-fill-drip"></i> 주요기능</li>
                 <li><i class="fas fa-fill-drip"></i> Front-end</li>
                 <li><i class="fas fa-fill-drip"></i> Back-end</li>
-                <li><i class="fas fa-fill-drip"></i> Git-hub</li>
+                <li><i class="fas fa-fill-drip"></i> Web-Site</li>
               </ul>
               </div>
 
               <div className='siteSkillBody'>
               <ul>
-                        <li>검색/필터/구매문의</li>
+                        <li>검색/조회/수정 등</li>
                         <li>JQUERY, HTML, CSS</li>
-                        <li>Eclipse, Java, MYSQL</li>
-                        <li>https://github.com/bogusipda</li>
+                        <li>SpringMVC, Java, MYSQL</li>
+                        <li><a href='https://dgis.dreamline.co.kr/sys/logout.do'>DGIS</a></li>
                       </ul>
               </div>
             </div>
@@ -496,22 +496,13 @@ function App() {
             </div>
             <div className='ProImg'>
               <img src={lawyer3} width='602' height='305' alt='lawyer3'/>
-
-
-
-              
-
             </div>
             
             <div className='ProContent'>
                 <div className='siteContent'>
                 상담을 웹, 앱으로 간편하게 진행 할 수 있는 변호사 상담 사이트입니다. <br/>
-                ERD설계와 클라이언트의 수정사항을 직접 UI기획 기획서를 작업하며 진행하며 유저들에게 보다 편리한 기능을 제공하는 것이 우선으로 진행하며, 
-                <br/>
-                <br/>UI의 화면에 기능들이 잘 배치되어질 수 있도록하기 위해 논의하며 사이트를 만들 때
-                중요한 순위와 코드 작성 시 <br/>기획과 기능구현 전 유스케이스의 중요성을 인식하며 개발할 때 불필요 데이터들을 줄일 수 있었으며 
-                <br/><br/>
-                MVP패턴으로 개발하며 상담신청부터 상담조회까지 CRUD 기능을 구현, 기존에 알던 방식과는 다른 JQuery 방식을 사용하며 지식범위를 넓힐 수 있었던 프로젝트였습니다.
+                ERD설계와 UI기획 기획서를 작업하며 유저들에게 편리한 기능을 제공하는 것이 우선으로 개발되기 위한 수정작업을 클라이언트와 상의,
+                MVP패턴으로 개발, 상담신청부터 상담조회까지 CRUD 기능을 구현과 JQUERY와 Css를 활용해 전체 레이아웃 작업하며 프로젝트 진행, 개발 시 주의해야할 점 등을 알게된 프로젝트였습니다.
                 </div>
 
                 <hr/>
@@ -522,17 +513,16 @@ function App() {
                 <li><i class="fas fa-fill-drip"></i> 주요기능</li>
                 <li><i class="fas fa-fill-drip"></i> Front-end</li>
                 <li><i class="fas fa-fill-drip"></i> Back-end</li>
-                <li><i class="fas fa-fill-drip"></i> Git-hub</li>
+                <li><i class="fas fa-fill-drip"></i> Web-site</li>
               </ul>
               </div>
 
               <div className='siteSkillBody'>
               <ul>
-                        <li>상담예약, 관리자 승인/거절 선택에 따라 상담진행/상담내역확인</li>
+                        <li>관리자 승인/거절, 상담진행/조회</li>
                         <li>JQuery, HTML, CSS</li>
-                        <li>Eclipse, Java, HeidiSQL</li>
-                        <li><Link to="https://devlop.senkorea.com:61238/intrcn/intrcnView.do">김기용 변호사</Link></li>
-                        <li><Route path="https://devlop.senkorea.com:61238/intrcn/intrcnView.do">김기용 변호사</Route></li>
+                        <li>SpringMVC, Java, HeidiSQL</li>
+                        <li><a href='https://devlop.senkorea.com:61238/intrcn/intrcnView.do'>김기용 변호사</a></li>
                       </ul>
               </div>
             </div>
@@ -557,7 +547,7 @@ function App() {
                 인사관리를 위한 통합 솔루션을 주제로, 사원들의 근태관리, 출퇴근, 휴가, 급여 등의 관리와 커뮤니티, 기록형 웹 사이트를 만들었습니다. 
                 <br/>
                 <br/>
-                spring에 필요한 설정파일, 라이브러리 등록하는 것에 시간이 많이 소요되었지만 프레임워크의 사용방법과 이클립스와 다른 DB연결 방법 등으로 spring의 장점을 알아갈 수 있었으며,
+                Spring에 필요한 설정파일, 라이브러리 등록하는 것에 시간이 많이 소요되었지만 프레임워크의 사용방법과 이클립스와 다른 DB연결 방법 등으로 spring의 장점을 알아갈 수 있었으며,
                 <br/>
                 실제 웹 화면과 유사한 기능을 제이쿼리로 구현하려면 어떻게 해야하는지에 대해 검색하며 익숙해지며 페이지 상황에 맞는 SQL를 구상하며 진행한 프로젝트였습니다.
                 </div>
@@ -576,11 +566,10 @@ function App() {
 
               <div className='siteSkillBody'>
               <ul>
-                        <li>검색/필터/구매문의</li>
-                        <li>React, TypeScript</li>
-                        <li>SpringBoot, Java, MySQL</li>
-                        <li>https://github.com/bogusipda</li>
-                        
+                        <li>사원등록/근태관리/게시판 등</li>
+                        <li>JQuery, HTML, CSS</li>
+                        <li>SpringMVC, Java, DBeaver</li>
+                        <li><a href='https://github.com/sky91119/oh_erp'>Oherp</a></li>
                       </ul>
               </div>
             </div>
@@ -605,6 +594,13 @@ function App() {
             <div className='ProContent'>
                 <div className='siteContent'>
                 일반적인 가구 판매를 넘어 우리집의 인테리어 견적을 믿고 맡길 수 있는 토탈 인테리어 솔루션을 제공하는 사이트
+                <br/>
+                <br/>
+                필터의 기능을 이용해 사용자가 원하는 제품만 볼 수 있도록, 회원가입자 한에서 선착순으로 댓글 입력 시 포인트 적립 기능 구현, 
+                DAO에 JDBC를 등록해 DB에 CURD 구현과 Servlet을 통해 서버에서 동작하는 JSP구성등을 
+                <br/>
+                개발하며 홈페이지 흐름도를 정확히 인지할 수 있었던 
+                팀프로젝트였습니다.
                 </div>
 
                 <hr/>
@@ -615,18 +611,16 @@ function App() {
                 <li><i class="fas fa-fill-drip"></i> 주요기능</li>
                 <li><i class="fas fa-fill-drip"></i> Front-end</li>
                 <li><i class="fas fa-fill-drip"></i> Back-end</li>
-                <li><i class="fas fa-fill-drip"></i> 도메인</li>
-                <li><i class="fas fa-fill-drip"></i> Git-hub</li>
+                <li><i class="fas fa-fill-drip"></i> Web-site</li>
               </ul>
               </div>
 
               <div className='siteSkillBody'>
               <ul>
-                        <li>필터 기능 후 제품 제공/구매/포인트 적립</li>
-                        <li>JavaScript</li>
+                        <li>필터/구매/포인트 적립 등</li>
+                        <li>JavaScript, HTML, CSS</li>
                         <li>Eclipse, Java, DBeaver</li>
-                        <li>http://www.sysout.co.kr/houudoin/</li>
-                        <li>https://github.com/SOL-hub/semi</li>
+                        <li><a href='http://www.sysout.co.kr/houudoin/'>houudoin</a></li>
                       </ul>
               </div>
             </div>
@@ -699,20 +693,27 @@ function App() {
                               <h5><i class="fas fa-edit"></i> 교육기관 : KH정보교육원(당산점)</h5>
                               <h5><i class="fas fa-edit"></i> 교육기간 : 20.04.16~20.09.23</h5>
                               <h5><i class="fas fa-edit"></i> 교육과목 : 디바이스&웹 콘텐츠 융합 디지털 컨버전스 양성과정(2)</h5>
-                                <h5><i class="fas fa-edit"></i> 교육내용 </h5>
+                                <h5><i class="fas fa-edit"></i> 교육내용1 </h5>
                                   <p>- ORACLE SQL Programming, DB 기본부터 활용까지</p>
                                   <p>- JAVA/ Servlet / JSP, jdbc/Spring Framework, Mybatis, Ajax</p>
                                   <p>- HTML / CSS / JavaScript/JQuery</p>
                                   <p>- 세미, 파이널팀 프로젝트 MVP패턴으로 진행</p>
-
+                                <br/>
+                                <h5><i class="fas fa-edit"></i> 프로젝트를 위한 개발 공부내용 </h5>  
+                                  <p>- Spring관련 교육 : Spring Boot와 JPA활용</p>
+                                  <p>- Spring Boot 기반으로 개발하는 Spring Security</p>
+                                  <p>- Spring Boot를 이용한 Restful Web Service 개발</p>
+                                
                               </div>
                             
                               <div className='ExperienceContent2'>
                               <h4>팀프로젝트 & 개인적 개발공부목록(최신 기록)</h4>
                               <h5><i class="fas fa-edit"></i> 팀프로젝트 : 사이드프로젝트(보드게임 중고거래 플랫폼)</h5> 
                                                                           <p>- 프론트엔드 REACT + typescript 개발</p>
+                                                                          <p>- Spring Boot와 JPA, Spring Security</p>
                               <h5><i class="fas fa-edit"></i> 개인적으로 공부하고 있는 프로그래밍 언어들</h5>
-                                  <p>- Spring Boot, JPA, JWT</p>
+                                  <p>- Spring Boot, JPA, JWT, Spring Security</p>
+                                  <p>- Restful Web Service 개발</p>
                                   <p>- JavaScript 객체지향 & ES6 신문법</p>
                                   <p>- React + typescript 활용</p>
                                   <p>- 정보처리기사 실기 시험준비 중</p>
